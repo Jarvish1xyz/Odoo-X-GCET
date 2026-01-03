e96b05d6125c91b29042a7d753ee8707d99c84e9
Milan Sinha <milans0415@gmail.com>
2026-01-03 11:45:35 +0000

    hy

---
+++ frontend/vite.config.js
@@ -0,0 +1,13 @@
+import { defineConfig } from 'vite'
+import tailwindcss from '@tailwindcss/vite'
+
+export default defineConfig({
+    server:{
+        proxy:{
+            '/api':""
+        },
+    },
+  plugins: [
+    tailwindcss(),
+  ],
+})
